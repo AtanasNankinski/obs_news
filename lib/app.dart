bb054bfe48c38266c7226a150ca8b3ed29dcf2bc
ANankinski <atanas.nankinski98@gmail.com>
2024-06-14 23:50:57 +0300

    Removed initial route and now app.dart is directly calling the splash screen.

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:obs_news/config_reader.dart';
 import 'package:obs_news/navigation/routing.dart';
 import 'package:obs_news/navigation/routing_constants.dart';
+import 'package:obs_news/splash_screen/splash_screen_page.dart';
 import 'package:obs_news/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: ConfigReader.isDebug(),
       onGenerateRoute: Routing.generateRoute,
-      initialRoute: RoutingConst.splashRoute,
+      //initialRoute: RoutingConst.splashRoute,
+      home: const SplashScreenPage(),
       theme: mainTheme,
     );
   }
